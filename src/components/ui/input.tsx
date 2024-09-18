77185c591e6a968ecaf4a040c39c0860aabdbe67
AkshayQa95 <akshaytembhurne95@gmail.com>
2024-09-18 21:29:49 +0530

    first commit

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 
-import { cn } from "@/lib/utils"
+//import {cn} from "./frontend/src/lib/utils.ts"
+import {cn} from "D:/Akshay/Sanvidha/Sanvidha/frontend/src/lib/utils.ts"
+//import { cn } from "@/lib/utils"
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
